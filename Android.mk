f7c82e49f7bb67935c3fa12986a6415c3c2c9050
winton.liu <admin@brahmaos.io>
2018-11-02 14:07:36 +0800

    Add MPermissionHelper activity
    Change-Id: I91d1e2837b698775a2e51df68b9d2cac1fec2602
    Signed-off-by: winton.liu <admin@brahmaos.io>

--- Android.mk
+++ Android.mk
@@ -7,5 +7,22 @@ LOCAL_SRC_FILES += unifiednlp-api/src/main/aidl/org/microg/nlp/api/LocationBacke
                    unifiednlp-api/src/main/aidl/org/microg/nlp/api/GeocoderBackend.aidl \
                    unifiednlp-api/src/main/aidl/org/microg/nlp/api/LocationCallback.aidl
 LOCAL_AIDL_INCLUDES := $(LOCAL_PATH)/unifiednlp-api/src/main/aidl
+LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/unifiednlp-api/src/main/res
+LOCAL_MANIFEST_FILE := unifiednlp-api/src/main/AndroidManifest.xml
 
 include $(BUILD_STATIC_JAVA_LIBRARY)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE_TAGS := optional
+LOCAL_SDK_VERSION := current
+LOCAL_PROGUARD_ENABLED := disabled
+LOCAL_DEX_PREOPT := false
+
+LOCAL_PACKAGE_NAME := MPermissionHelper
+LOCAL_SRC_FILES := unifiednlp-api/src/main/java/org/microg/nlp/api/MPermissionHelperActivity.java
+LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/unifiednlp-api/src/main/res
+LOCAL_MANIFEST_FILE := unifiednlp-api/src/main/AndroidManifest.xml
+
+include $(BUILD_PACKAGE)
+
